63a6e8a943bbb7c092d1f19e655989b7e6b104a2
Gorapower <Alexis@Alfredos-MacBook-Pro.local>
2015-01-28 19:16:34 -0600

    backgroud listo
    acomode background y el suelo

--- main.lua
+++ main.lua
@@ -5,20 +5,21 @@
 -----------------------------------------------------------------------------------------
 
 -- Your code here
+display.setStatusBar( display.HiddenStatusBar )
 display.setDefault( "textureWrapX", "repeat" )
 display.setDefault( "textureWrapY", "repeat" )
 
 local x,y = display.contentCenterX, display.contentCenterY
-local o = display.newRect( x, y, display.contentWidth, display.contentHeight )
+local o = display.newRect( x, (y-45), (display.viewableContentWidth + 50), display.viewableContentHeight )
 o.fill = { type="image", filename="assets/bg.png" }
--- o.fill.scaleX = 0.1
--- o.fill.scaleY = 0.1
+ o.fill.scaleX = 1
+ o.fill.scaleY = 1
 
 display.setDefault( "textureWrapX", "repeat" )
 display.setDefault( "textureWrapY", "repeat" )
 
 local x,y = display.contentCenterX, display.contentCenterY
-local o = display.newRect(x,650, display.contentWidth, display.contentHeight )
-o.fill = { type="image", filename="assets/ground.png" }
--- o.fill.scaleX = 0.1
--- o.fill.scaleY = 0.1
+local u = display.newRect(x,480, display.contentWidth, 150 )
+u.fill = { type="image", filename="assets/ground.png" }
+ u.fill.scaleX = 1
+ u.fill.scaleY = 1
