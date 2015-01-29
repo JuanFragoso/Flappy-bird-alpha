adc5f2c3bcf69bf96737d11ecd5b79fddb863c0b
Juan Fragoso <juan_fragoso@me.com>
2015-01-28 18:37:35 -0600

    poner bg y ground

--- main.lua
+++ main.lua
@@ -5,3 +5,23 @@
 -----------------------------------------------------------------------------------------
 
 -- Your code here
+display.setDefault( "textureWrapX", "repeat" )
+display.setDefault( "textureWrapY", "repeat" )
+
+local x,y = display.contentCenterX, display.contentCenterY
+local o = display.newRect( x, y, display.contentWidth, display.contentHeight )
+o.fill = { type="image", filename="assets/bg.png" }
+-- o.fill.scaleX = 0.1
+-- o.fill.scaleY = 0.1
+
+display.setDefault( "textureWrapX", "repeat" )
+display.setDefault( "textureWrapY", "repeat" )
+
+local x,y = display.contentCenterX, display.contentCenterY
+local o = display.newRect(x,650, display.contentWidth, display.contentHeight )
+
+o.display.newImage("assets/ground.png")
+
+-- o.fill = { type="image", filename="assets/ground.png" }
+-- o.fill.scaleX = 0.1
+-- o.fill.scaleY = 0.1
