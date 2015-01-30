39a5adf5ac1c20014dae1fa33cba168996eea764
Gorapower <Alexis@Alfredos-MacBook-Pro.local>
2015-01-30 00:05:20 -0600

    ya funcionan las esenas
    escenas funcionales, solo hasta start.

--- main.lua
+++ main.lua
@@ -1,130 +1,106 @@
------------------------------------------------------------------------------------------
---
 -- main.lua
---
------------------------------------------------------------------------------------------
 
--- Your code here
 display.setStatusBar( display.HiddenStatusBar )
-display.setDefault( "textureWrapX", "repeat" )
-display.setDefault( "textureWrapY", "repeat" )
 
-local x,y = display.contentCenterX, display.contentCenterY
-local bg = display.newRect( x, (y-45), (display.viewableContentWidth + 50), (display.viewableContentHeight + 100) )
-bg.fill = { type="image", filename="assets/bg.png" }
-
-display.setDefault( "textureWrapX", "repeat" )
-display.setDefault( "textureWrapY", "repeat" )
-
-local x,y = display.contentCenterX, display.contentCenterY
-local back1 = display.newRect(x,480, (display.viewableContentWidth + 50), 150 )
-back1.fill = { type="image", filename="assets/ground.png" }
-local back2 = display.newRect((x*2),480, (display.viewableContentWidth + 50), 150 )
-back2.fill = { type="image", filename="assets/ground.png" }
-local back3 = display.newRect((x*3),480, (display.viewableContentWidth + 50), 150 )
-back3.fill = { type="image", filename="assets/ground.png" }
-local back4 = display.newRect((x*4),480, (display.viewableContentWidth + 50), 150 )
-back4.fill = { type="image", filename="assets/ground.png" }
-local titulo = display.newRect( x, (y-100), (display.viewableContentWidth/2), 80 )
-titulo.fill = { type="image", filename="assets/titulo.png" }
-local play = display.newRect( x, (y+130), (display.viewableContentWidth/3), 75 )
-play.fill = { type="image", filename="assets/play.png" }
+local composer = require("composer")
+composer.gotoScene( "start" )
 
 
- local function scrollBackground(event)
-    local xOffset = -8
+-- codigo a mover
+
+-- display.setDefault( "textureWrapX", "repeat" )
+-- display.setDefault( "textureWrapY", "repeat" )
+
+-- local x,y = display.contentCenterX, display.contentCenterY
+-- local bg = display.newRect( x, (y-45), (display.viewableContentWidth + 50), (display.viewableContentHeight + 100) )
+-- bg.fill = { type="image", filename="assets/bg.png" }
+-- local back1 = display.newRect(x,480, (display.viewableContentWidth + 50), 150 )
+-- back1.fill = { type="image", filename="assets/ground.png" }
+-- local back2 = display.newRect((x*2),480, (display.viewableContentWidth + 50), 150 )
+-- back2.fill = { type="image", filename="assets/ground.png" }
+-- local back3 = display.newRect((x*3),480, (display.viewableContentWidth + 50), 150 )
+-- back3.fill = { type="image", filename="assets/ground.png" }
+-- local back4 = display.newRect((x*4),480, (display.viewableContentWidth + 50), 150 )
+-- back4.fill = { type="image", filename="assets/ground.png" }
+-- local titulo = display.newRect( x, (y-100), (display.viewableContentWidth/2), 80 )
+-- titulo.fill = { type="image", filename="assets/titulo.png" }
+-- local play = display.newRect( x, (y+130), (display.viewableContentWidth/3), 75 )
+-- play.fill = { type="image", filename="assets/play.png" }
+
+
+--  local function scrollBackground(event)
+--     local xOffset = -8
     
-    back1.x = back1.x + xOffset
-    back2.x = back2.x + xOffset
-    back3.x = back3.x + xOffset
-    back4.x = back4.x + xOffset
-    if (back1.x) < -(display.contentWidth) then
-        back1.x = (display.contentWidth*1.55)
-    end
-    if (back2.x) < -(display.contentWidth) then
-        back2.x = (display.contentWidth*1.55)
-    end
-    if (back3.x) < -(display.contentWidth) then
-        back3.x = (display.contentWidth*1.55)
-    end
-    if (back4.x) < -(display.contentWidth) then
-        back4.x = (display.contentWidth*1.55)
-    end
+--     back1.x = back1.x + xOffset
+--     back2.x = back2.x + xOffset
+--     back3.x = back3.x + xOffset
+--     back4.x = back4.x + xOffset
+--     if (back1.x) < -(display.contentWidth) then
+--         back1.x = (display.contentWidth*1.55)
+--     end
+--     if (back2.x) < -(display.contentWidth) then
+--         back2.x = (display.contentWidth*1.55)
+--     end
+--     if (back3.x) < -(display.contentWidth) then
+--         back3.x = (display.contentWidth*1.55)
+--     end
+--     if (back4.x) < -(display.contentWidth) then
+--         back4.x = (display.contentWidth*1.55)
+--     end
     
-end
+-- end
 
-Runtime:addEventListener( "enterFrame", scrollBackground )
-
-
+-- Runtime:addEventListener( "enterFrame", scrollBackground )
 
 
 
-p_options = 
-    {
-        -- Required params
-        width = 36,
-        height = 26,
-        numFrames = 3,
-        -- content scaling
-        sheetContentWidth = 108,
-        sheetContentHeight = 26,
-    }
 
 
--- local sheet_runningCat = graphics.newImageSheet( "assets/bird.png", p_options )
--- local sequences_runningCat = {
---     -- consecutive frames sequence
+-- p_options = 
 --     {
---         name = "normalRun",
---         start = 1,
---         count = 3,
---         time = 500,
---         loopCount = 0,
---         loopDirection = "forward"
+--         -- Required params
+--         width = 36,
+--         height = 26,
+--         numFrames = 3,
+--         -- content scaling
+--         sheetContentWidth = 108,
+--         sheetContentHeight = 26,
 --     }
--- }
+-- local playerSheet = graphics.newImageSheet( "assets/bird.png", p_options )
+-- local bird = display.newSprite( playerSheet, { name="player", start=1, count=3, time=500, loopCount = 0 } )
 
--- sequences_runningCat.x = 200
--- sequences_runningCat.y = 200
-
-local playerSheet = graphics.newImageSheet( "assets/bird.png", p_options )
-local bird = display.newSprite( playerSheet, { name="player", start=1, count=3, time=500, loopCount = 0 } )
-
-bird.x = 150
-bird.y = 200
-bird:play()
+-- bird.x = 150
+-- bird.y = 200
+-- bird:play()
 
  
-bird.anchorX = 0.5
-bird.anchorY = 0.5
+-- bird.anchorX = 0.5
+-- bird.anchorY = 0.5
 
-vs = 0
-gvt = 6
+-- vs = 0
+-- gvt = 6
 
---player:translate(display.contentCenterX, display.contentCenterY)
-
-function o:mouse(event)
-    if event.isPrimaryButtonDown then
-        --ab.y = ab.y - 150
-        vs = 30
+-- function bg:mouse(event)
+--     if event.isPrimaryButtonDown then
+--         vs = 30
   
-    end
-end
+--     end
+-- end
 
-o:addEventListener('mouse', bg)
+-- bg:addEventListener('mouse', bg)
 
-local function gravity (event)
-    movementParams = {
-        x = bird.x,
-        y = bird.y - vs,
-        time = 175
-    }
-    transition.moveTo(bird, movementParams)
-    vs = vs - gvt
+-- local function gravity (event)
+--     movementParams = {
+--         x = bird.x,
+--         y = bird.y - vs,
+--         time = 175
+--     }
+--     transition.moveTo(bird, movementParams)
+--     vs = vs - gvt
 
-end
+-- end
 
-timer.performWithDelay(50, gravity, -1)
+-- timer.performWithDelay(50, gravity, -1)
 
 
 
---
+++ start.lua
@@ -0,0 +1,100 @@
+-- Start.lua
+
+local composer = require( "composer" )
+local scene = composer.newScene()
+local physics = require "physics"
+physics.start()
+
+-- funciones
+function game(event)
+     if event.phase == "ended" then
+        composer.gotoScene("game")
+     end
+end
+function scrollground(event)
+    local xOffset = -8
+    ground1.x = ground1.x + xOffset
+    ground2.x = ground2.x + xOffset
+    ground3.x = ground3.x + xOffset
+    ground4.x = ground4.x + xOffset
+    if (ground1.x) < -(display.contentWidth) then
+        ground1.x = (display.contentWidth*1.55)
+    end
+    if (ground2.x) < -(display.contentWidth) then
+        ground2.x = (display.contentWidth*1.55)
+    end
+    if (ground3.x) < -(display.contentWidth) then
+        ground3.x = (display.contentWidth*1.55)
+    end
+    if (ground4.x) < -(display.contentWidth) then
+        ground4.x = (display.contentWidth*1.55)
+    end 
+end
+
+--iniciliazar start
+function scene:create( event )
+   local sceneGroup = self.view
+   display.setDefault( "textureWrapX", "repeat" )
+   display.setDefault( "textureWrapY", "repeat" )
+   local x,y = display.contentCenterX, display.contentCenterY
+   background = display.newRect( x, (y-45), (display.viewableContentWidth + 50), (display.viewableContentHeight + 100) )
+   background.fill = { type="image", filename="assets/bg.png" }
+   sceneGroup:insert(background)
+   ground1 = display.newRect(x,480, (display.viewableContentWidth + 50), 150 )
+   physics.addBody(ground1, "static", {density=.1, bounce=0.1, friction=.2})
+   ground1.fill = { type="image", filename="assets/ground.png" }
+   sceneGroup:insert(ground1)
+   ground2 = display.newRect((x*2),480, (display.viewableContentWidth + 50), 150 )
+   physics.addBody(ground2, "static", {density=.1, bounce=0.1, friction=.2})
+   ground2.fill = { type="image", filename="assets/ground.png" }
+   sceneGroup:insert(ground2)
+   ground3 = display.newRect((x*3),480, (display.viewableContentWidth + 50), 150 )
+   physics.addBody(ground3, "static", {density=.1, bounce=0.1, friction=.2})
+   ground3.fill = { type="image", filename="assets/ground.png" }
+   sceneGroup:insert(ground3)
+   ground4 = display.newRect((x*4),480, (display.viewableContentWidth + 50), 150 )
+   physics.addBody(ground4, "static", {density=.1, bounce=0.1, friction=.2})
+   ground4.fill = { type="image", filename="assets/ground.png" }
+   sceneGroup:insert(ground4)
+   titulo = display.newRect( x, (y-100), (display.viewableContentWidth/2), 80 )
+   titulo.fill = { type="image", filename="assets/titulo.png" }
+   sceneGroup:insert(titulo)
+   playb = display.newRect( x, (y+130), (display.viewableContentWidth/3), 75 )
+   playb.fill = { type="image", filename="assets/play.png" }
+   sceneGroup:insert(playb)
+end
+
+-- iniciar funciones
+function scene:show( event )
+   local sceneGroup = self.view
+   local phase = event.phase
+   if ( phase == "will" ) then
+   elseif ( phase == "did" ) then
+        playb:addEventListener("touch", game)
+        Runtime:addEventListener( "enterFrame", scrollground )
+   end
+end
+
+-- finalizar funciones
+function scene:hide( event )
+   local sceneGroup = self.view
+   local phase = event.phase
+   if ( phase == "will" ) then
+        playb:removeEventListener("touch", game)
+        Runtime:removeEventListener( "enterFrame", scrollground )
+   elseif ( phase == "did" ) then
+   end
+end
+
+--finalizar start
+function scene:destroy( event )
+   local sceneGroup = self.view
+end
+
+-- listeners
+scene:addEventListener( "create", scene )
+scene:addEventListener( "show", scene )
+scene:addEventListener( "hide", scene )
+scene:addEventListener( "destroy", scene )
+
+return scene
