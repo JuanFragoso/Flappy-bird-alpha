c681444d753c8992cc5b27dd2a3e1d9bdc59bcd7
Gorapower <Alexis@Alfredos-MacBook-Pro.local>
2015-01-30 18:30:21 -0600

    juego final
    falta final y score

--- game.lua
+++ game.lua
@@ -8,7 +8,7 @@ lose = false
 
 -- funciones
 function scrollground(event)
-    local xOffset = -8
+    local xOffset = -5
     if lose == false then
      ground1.x = ground1.x + xOffset
      ground2.x = ground2.x + xOffset
@@ -36,37 +36,37 @@ function scrollground(event)
         ground4.x = (display.contentWidth*1.55)
     end 
     if (tube1.x) < -(display.contentWidth) then
-    	tube1.y =coordenadas[math.random (22)]
+    	tube1.y =coordenadas[math.random (20)]
         tube1.x = (display.contentWidth*1.25)
     end
     if (tube2.x) < -(display.contentWidth) then
-    	tube2.y =tube1.y+500
+    	tube2.y =tube1.y+520
     	tube2.x = (display.contentWidth*1.25)
     end
     if (tube3.x) < -(display.contentWidth) then
-    	tube3.y =coordenadas[math.random (22)]
+    	tube3.y =coordenadas[math.random (20)]
     	tube3.x = (display.contentWidth*1.25)
     end
     if (tube4.x) < -(display.contentWidth) then
-    	tube4.y =tube3.y+500
+    	tube4.y =tube3.y+520
     	tube4.x = (display.contentWidth*1.25)
     end
     if (tube5.x) < -(display.contentWidth) then
-    	tube5.y =coordenadas[math.random (22)]
+    	tube5.y =coordenadas[math.random (20)]
     	tube5.x = (display.contentWidth*1.25)
     end
     if (tube6.x) < -(display.contentWidth) then
-    	tube6.y =tube5.y+500
+    	tube6.y =tube5.y+520
     	tube6.x = (display.contentWidth*1.25)
     end
 end
 function fly(event)
   if playing == false then
-    bird:applyForce( 0,-400, bird.x, bird.y )
-    physics.setGravity(0,80)
+    bird:setLinearVelocity( 0,-300 )
+    physics.setGravity(0,50)
     playing = true
   else
-    bird:applyForce( 0,-400, bird.x, bird.y )
+    bird:setLinearVelocity( 0,-300 )
   end
 end
 function onCollision( event )
@@ -85,37 +85,37 @@ function scene:create( event )
    background = display.newRect( x, (y-45), (display.viewableContentWidth + 50), (display.viewableContentHeight + 100) )
    background.fill = { type="image", filename="assets/bg.png" }
    sceneGroup:insert(background)
-   coordenadas = {-130,-120,-110,-100,-90,-80,-70,-60,-50,-40,-30,-20,-10,0,10,20,30,40,50,60,70,80}
-   tube1 = display.newRect(display.contentWidth*1.25,coordenadas[math.random (22)], (display.viewableContentWidth / 6), 420 )
+   coordenadas = {-130,-120,-110,-100,-90,-80,-70,-60,-50,-40,-30,-20,-10,0,10,20,30,40,50,60}
+   tube1 = display.newRect(display.contentWidth*1.25,coordenadas[math.random (20)], (display.viewableContentWidth / 6), 420 )
    tube1.fill = { type="image", filename="assets/tube1.png" }
    physics.addBody(tube1, "kinematic",{density=1, bounce=0.1, friction=.2})
    tube1.isSleepingAllowed = false
    sceneGroup:insert(tube1)
    tube1.myName = "tube1"
-   tube2 = display.newRect(display.contentWidth*1.25,tube1.y+500, (display.viewableContentWidth / 6 ), 420 )
+   tube2 = display.newRect(display.contentWidth*1.25,tube1.y+520, (display.viewableContentWidth / 6 ), 420 )
    tube2.fill = { type="image", filename="assets/tube2.png" }
    physics.addBody(tube2, "kinematic",{density=1, bounce=0.1, friction=.2})
    sceneGroup:insert(tube2)
    tube2.myName = "tube2"
    tube2.isSleepingAllowed = false
-   tube3 = display.newRect(display.contentWidth*2,coordenadas[math.random (22)], (display.viewableContentWidth / 6), 420 )
+   tube3 = display.newRect(display.contentWidth*2,coordenadas[math.random (20)], (display.viewableContentWidth / 6), 420 )
    tube3.fill = { type="image", filename="assets/tube1.png" }
    physics.addBody(tube3, "kinematic",{density=1, bounce=0.1, friction=.2})
    sceneGroup:insert(tube3)
    tube3.myName = "tube3"
    tube3.isSleepingAllowed = false
-   tube4 = display.newRect(display.contentWidth*2,tube3.y+500, (display.viewableContentWidth / 6 ), 420 )
+   tube4 = display.newRect(display.contentWidth*2,tube3.y+520, (display.viewableContentWidth / 6 ), 420 )
    tube4.fill = { type="image", filename="assets/tube2.png" }
    physics.addBody(tube4, "kinematic",{density=1, bounce=0.1, friction=.2})
    sceneGroup:insert(tube4)
    tube4.myName = "tube4"
-   tube5 = display.newRect(display.contentWidth*2.75,coordenadas[math.random (22)], (display.viewableContentWidth / 6), 420 )
+   tube5 = display.newRect(display.contentWidth*2.75,coordenadas[math.random (20)], (display.viewableContentWidth / 6), 420 )
    tube5.fill = { type="image", filename="assets/tube1.png" }
    physics.addBody(tube5, "kinematic",{density=1, bounce=0.1, friction=.2})
    sceneGroup:insert(tube5)
    tube5.myName = "tube5"
    tube5.isSleepingAllowed = false
-   tube6 = display.newRect(display.contentWidth*2.75,tube5.y+500, (display.viewableContentWidth / 6 ), 420 )
+   tube6 = display.newRect(display.contentWidth*2.75,tube5.y+520, (display.viewableContentWidth / 6 ), 420 )
    tube6.fill = { type="image", filename="assets/tube2.png" }
    physics.addBody(tube6, "kinematic",{density=1, bounce=0.1, friction=.2})
    sceneGroup:insert(tube6)
@@ -155,7 +155,7 @@ function scene:create( event )
    }
    playerSheet = graphics.newImageSheet( "assets/bird.png", p_options )
    bird = display.newSprite( playerSheet, { name="player", start=1, count=3, time=500, loopCount = 0 } )
-   bird.x = 150
+   bird.x = 130
    bird.y = 200
    bird:play()
    physics.addBody(bird,{ density=1.0, friction=0.3, bounce=0.2 })
@@ -163,9 +163,6 @@ function scene:create( event )
    bird.anchorX = 0.5
    bird.anchorY = 0.5
    sceneGroup:insert(bird)
-   vs = 0
-   gvt = 6
-
 end
 
 -- iniciar funciones
